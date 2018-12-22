667930d91c34a42b10636b30daf95b889915edb6
Matthew Hallberg <matthewhallberg@gmail.com>
2018-12-22 02:54:24 -0500

    added text to speech android plugin

---
+++ Assets/README.txt
@@ -0,0 +1,18 @@
+THANK YOU FOR YOUR PURCHASE
+
+Infinity Engine is written using C# 7 syntax. This means that you must activate this option in Unity Editor.
+
+If you don't do this, Unity will not compile your project.
+
+To enable C# 7 syntax, go to https://forum.unity.com/threads/unity-incremental-c-compiler.523993/
+
+Then switch the target Scripting Runtime Version to .NET 4* in Player Settings 
+
+THE FULL DOCUMENTATION IS AVAILABLE AT :
+
+https://infinity-engine-f6f33.firebaseapp.com/#Speech-Engine
+
+IF YOU LIKE THIS ASSET, PLEASE RATE IT AT http://u3d.as/riS
+
+IF YOU HAVE ANY QUESTION/SUGGEST, PLEASE CONTACT ME AT  mciissee@gmail.com
+
---
+++ Assets/Scripts/VoiceController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VoiceController : MonoBehaviour {
+
+    public BrowserView browser;
+
+    public void onActivityResult(string recognizedText) {
+        char[] delimiterChars = { '~' };
+        string[] result = recognizedText.Split(delimiterChars);
+        browser.SearchGoogle(result[0]);
+    }
+
+    public void GetSpeech() {
+
+        AndroidJavaClass pluginClass = new AndroidJavaClass("com.plugin.speech.pluginlibrary.TestPlugin");
+        Debug.Log("Call 1 Started");
+
+        // Pass the name of the game object which has the onActivityResult(string recognizedText) attached to it.
+        pluginClass.CallStatic("setReturnObject", "VoiceToText");
+        Debug.Log("Return Object Set");
+
+
+        // Setting language is optional. If you don't run this line, it will try to figure out language based on device settings
+        pluginClass.CallStatic("setLanguage", "en_US");
+        Debug.Log("Language Set");
+
+
+        // The following line sets the maximum results you want for recognition
+        pluginClass.CallStatic("setMaxResults", 3);
+        Debug.Log("Max Results Set");
+
+        // The following line sets the question which appears on intent over the microphone icon
+        pluginClass.CallStatic("changeQuestion", "Hello, How can I help you???");
+        Debug.Log("Question Set");
+
+
+        Debug.Log("Call 2 Started");
+
+        // Calls the function from the jar file
+        pluginClass.CallStatic("promptSpeechInput");
+
+        Debug.Log("Call End");
+    }
+}
