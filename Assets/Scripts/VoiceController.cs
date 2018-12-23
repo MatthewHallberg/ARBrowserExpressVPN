396fd42726120c12713472070d21c352f451048e
Matthew Hallberg <matthewhallberg@gmail.com>
2018-12-23 00:23:04 -0500

    new plugin working with no speech modal, but ap crashes on detection error commiting before I rebuild speech library.

--- Assets/Scripts/VoiceController.cs
+++ Assets/Scripts/VoiceController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class VoiceController : MonoBehaviour {
@@ -8,44 +6,40 @@ public class VoiceController : MonoBehaviour {
     public BrowserView browser;
     public Text debugText;
 
-    AndroidJavaClass pluginClass = new AndroidJavaClass("com.plugin.speech.pluginlibrary.TestPlugin");
+    AndroidJavaObject activity;
+    AndroidJavaObject plugin;
 
     private void Start() {
         InitPlugin();
     }
 
     void InitPlugin() {
-        // Pass the name of the game object which has the onActivityResult(string recognizedText) attached to it.
-        pluginClass.CallStatic("setReturnObject", "VoiceToText");
-        Debug.Log("Return Object Set");
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
 
+        activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        
+        activity.Call("runOnUiThread", new AndroidJavaRunnable(() => {
+                plugin = new AndroidJavaObject(
+                "com.example.matthew.plugin.VoiceBridge");
+        }));
 
-        // Setting language is optional. If you don't run this line, it will try to figure out language based on device settings
-        pluginClass.CallStatic("setLanguage", "en_US");
-        Debug.Log("Language Set");
-
-
-        // The following line sets the maximum results you want for recognition
-        pluginClass.CallStatic("setMaxResults", 3);
-        Debug.Log("Max Results Set");
-
-        // The following line sets the question which appears on intent over the microphone icon
-        pluginClass.CallStatic("changeQuestion", "Hello, How can I help you???");
-        Debug.Log("Question Set");
+        activity.Call("runOnUiThread", new AndroidJavaRunnable(() => {
+            plugin.Call("StartPlugin");
+        }));
     }
 
-    public void onActivityResult(string recognizedText) {
-        char[] delimiterChars = { '~' };
-        string[] result = recognizedText.Split(delimiterChars);
+    public void OnVoiceResult(string recognizedText) {
 //        browser.ProcessQuery(result[0]);
-        debugText.text = result[0];
+        debugText.text = recognizedText;
     }
 
     public void GetSpeech() {
         //play loading animation
-//      AnimLoading.Instance.ShouldLoad(true);
+        //      AnimLoading.Instance.ShouldLoad(true);
 
         // Calls the function from the jar file
-        pluginClass.CallStatic("promptSpeechInput");
+        activity.Call("runOnUiThread", new AndroidJavaRunnable(() => {
+            plugin.Call("StartSpeaking");
+        }));
     }
 }
