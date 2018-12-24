84c6cab29b1add503fd0c7f4789244a36b991f97
Matthew Hallberg <matthewhallberg@gmail.com>
2018-12-24 17:05:48 -0500

    Added another node.js server for getting links of google results...works fairly well.

--- Assets/Scripts/BrowserView.cs
+++ Assets/Scripts/BrowserView.cs
@@ -10,8 +10,8 @@ using System.IO;
 public class BrowserView : MonoBehaviour {
 
     const string GOOGLE_SEARCH = "https://www.google.com/search?q=";
-    const string GET_LINKS = "https://www.matthewhallberg.com/getLinks.php";
-    const string URI = "http://198.252.105.8:3000/getImage";
+    const string URI_IMAGE = "http://198.252.105.8:3000/getImage";
+    const string URI_LINKS = "http://198.252.105.8:12001";
 
     public Material screenMat;
     public Texture whiteTex;
@@ -43,28 +43,27 @@ public class BrowserView : MonoBehaviour {
     void SearchGoogle(string query) {
         string currUrl = GOOGLE_SEARCH + query;
         StartCoroutine(GetImageFromURL(currUrl));
-        StartCoroutine(GetLinksFromURL(currUrl));
+        StartCoroutine(GetLinksFromQuery(query));
     }
 
-    IEnumerator GetLinksFromURL(string url) {
-        WWWForm form = new WWWForm();
-        //form.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
-        form.AddField("url", url);
-        UnityWebRequest www = UnityWebRequest.Post(GET_LINKS, form);
+    IEnumerator GetLinksFromQuery(string query) {
+        UnityWebRequest www = UnityWebRequest.Get(URI_LINKS);
+        www.SetRequestHeader("head", UnityWebRequest.EscapeURL(query));
         yield return www.SendWebRequest();
         if (www.isNetworkError || www.isHttpError) {
             Debug.Log(www.error);
         } else {
-            print(www.downloadHandler.text);
-
-            //string[] html = www.downloadHandler.text.Split('\n');
-
-
+            string[] links = www.downloadHandler.text.Split(',');
+            foreach (string link in links) {
+                if (link.Length > 0) {
+                    print(link);
+                }
+            }
         }
     }
 
     IEnumerator GetImageFromURL(string url) {
-        UnityWebRequest www = UnityWebRequest.Get(URI);
+        UnityWebRequest www = UnityWebRequest.Get(URI_IMAGE);
         www.SetRequestHeader("head", url);
         yield return www.SendWebRequest();
         if (www.isNetworkError || www.isHttpError) {
