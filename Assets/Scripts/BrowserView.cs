27c7e418a71fbe3da6119c7a62a73929e4575956
Matthew Hallberg <matthewhallberg@gmail.com>
2018-12-24 04:33:47 -0500

    modified UI, started trying to get links back, not working yet.

--- Assets/Scripts/BrowserView.cs
+++ Assets/Scripts/BrowserView.cs
@@ -10,6 +10,7 @@ using System.IO;
 public class BrowserView : MonoBehaviour {
 
     const string GOOGLE_SEARCH = "https://www.google.com/search?q=";
+    const string GET_LINKS = "https://www.matthewhallberg.com/getLinks.php";
     const string URI = "http://198.252.105.8:3000/getImage";
 
     public Material screenMat;
@@ -42,6 +43,24 @@ public class BrowserView : MonoBehaviour {
     void SearchGoogle(string query) {
         string currUrl = GOOGLE_SEARCH + query;
         StartCoroutine(GetImageFromURL(currUrl));
+        StartCoroutine(GetLinksFromURL(currUrl));
+    }
+
+    IEnumerator GetLinksFromURL(string url) {
+        WWWForm form = new WWWForm();
+        //form.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
+        form.AddField("url", url);
+        UnityWebRequest www = UnityWebRequest.Post(GET_LINKS, form);
+        yield return www.SendWebRequest();
+        if (www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        } else {
+            print(www.downloadHandler.text);
+
+            //string[] html = www.downloadHandler.text.Split('\n');
+
+
+        }
     }
 
     IEnumerator GetImageFromURL(string url) {
