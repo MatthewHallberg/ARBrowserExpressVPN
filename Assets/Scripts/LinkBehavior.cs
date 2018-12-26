ac1776331ca3948858780e62cec6e70c1ff4300d
Matthew Hallberg <matthewhallberg@gmail.com>
2018-12-26 03:16:08 -0500

    added animations and sounds to links.

--- Assets/Scripts/LinkBehavior.cs
+++ Assets/Scripts/LinkBehavior.cs
@@ -4,7 +4,25 @@ using UnityEngine;
 
 public class LinkBehavior : MonoBehaviour {
 
+    public AudioSource audioSource;
+    public AudioClip growSound;
+    public AudioClip clickSound;
+
+    Vector3 originalSize;
+
+    private void Awake() {
+        audioSource.PlayOneShot(growSound);
+        originalSize = transform.localScale;
+        transform.localScale = Vector3.zero;
+    }
+
+    private void Update() {
+        transform.localScale = Vector3.Lerp(transform.localScale, originalSize, Time.deltaTime * 6f);
+    }
+
     private void OnMouseDown() {
+        audioSource.PlayOneShot(clickSound);
+        transform.localScale = originalSize / 2f;
         Texture thisTexture = GetComponent<Renderer>().material.mainTexture;
         SceneController.Instance.screenMat.mainTexture = thisTexture;
     }
